788cda357c1c1a641b73681b76d643940ef79628
Mohamed <salman@louna.tv>
2024-08-06 16:03:32 +0400

    minor changes

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-import { initializeApp, applicationDefault } from "firebase-admin/app";
+import { initializeApp, cert} from "firebase-admin/app";
 import { getMessaging } from "firebase-admin/messaging";
 import express, { json, response } from "express"
 import bodyParser from "body-parser";
+import dotenv from 'dotenv';
 import cors from 'cors';
 const app = express()
+dotenv.config();
 
 app.use(express.json())
+
+console.log(process.env.FIREBASE_UNIVERSE_DOMAIN,"++")
 
 // middlewares
 app.use(bodyParser.urlencoded({
@@ -16,18 +20,28 @@ app.use(cors())
 
 
 initializeApp({
-  credential: applicationDefault(),
+  credential:cert({
+    type:process.env.FIREBASE_TYPE,
+    project_id:process.env.FIREBASE_PROJECT_ID,
+    private_key_id:process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key:process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email:process.env.FIREBASE_CLIENT_EMAIL,
+    client_id:process.env.FIREBASE_CLIENT_ID,
+    auth_uri:process.env.FIREBASE_AUTH_URI,
+    token_uri:process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url:process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url:process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    universe_domain:process.env.FIREBASE_UNIVERSE_DOMAIN 
+  }),
   projectId: "qai-tanara"
 })
 
 
+
 app.post("/send", (req, res) => {
-  const {fcmtoken}= req.body
+  const {fcmtoken,notification}= req.body
   const message = {
-    notification: {
-      title: "Good Morning",
-      body: "Game Show is Live Now"
-    },
+    notification:notification,
     token:fcmtoken
   };
   getMessaging().send(message)
